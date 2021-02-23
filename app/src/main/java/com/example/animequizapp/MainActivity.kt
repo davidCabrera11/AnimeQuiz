f293e43ce886fe703019ce7cfbe743d3b2f04583
David Cabrera <davidcabrera052196@gmail.com>
2021-02-22 20:05:50 -0500

    Set on click listener for start button

--- app/src/main/java/com/example/animequizapp/MainActivity.kt
+++ app/src/main/java/com/example/animequizapp/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.animequizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.animequizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: ConstraintLayout = binding.root
+        setContentView(view)
+
+        binding.btnStart.setOnClickListener{
+
+            Toast.makeText(this, "Start button clicked",Toast.LENGTH_LONG).show()
+
+        }
 
 
 
